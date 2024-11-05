ae411eb86ef7989277c2654b2dc255ee3c7b13fc
Gabor Vilics <131352641+Naurid@users.noreply.github.com>
2024-11-05 15:56:16 +0100

    Exercice 2 done

---
+++ Exercice1/Program.cs
@@ -0,0 +1,94 @@
+﻿#region Console
+
+// string name = "John";
+//
+// Console.WriteLine($"Bonjour {name},il est {DateTime.Now.ToShortDateString()}");
+//
+// string response = Console.ReadLine()?? "no name input";
+//
+// Console.WriteLine(response);
+
+#endregion
+
+#region Exercices
+//parse  method
+while(true){
+    Console.WriteLine("Write the first number:");
+    string? firstNumber = Console.ReadLine();
+    float number1;
+    
+    try
+    {
+        number1 = float.Parse(firstNumber);
+    }
+    catch (Exception e)
+    {
+        WriteInColor($"{firstNumber} n'est pas un nombre", ConsoleColor.Red);
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+    Console.WriteLine("Write the second number:");
+    string? secondNumber = Console.ReadLine();
+    float number2 ;
+    
+    try
+    {
+        number2 = float.Parse(secondNumber);
+    }
+    catch (Exception e)
+    {
+        WriteInColor($"{secondNumber} n'est pas un nombre", ConsoleColor.Red);
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+
+    //WriteInColor($"{number1} + {number2} = {number1 + number2}", ConsoleColor.Green);
+    break;
+}
+
+//tryparse method
+while(true){
+    Console.WriteLine("Write the first number:");
+    string? firstNumber = Console.ReadLine();
+    float number1;
+    bool isNumber1 = float.TryParse(firstNumber, out number1);
+
+    if (!isNumber1)
+    {
+        WriteInColor($"{firstNumber} n'est pas un nombre", ConsoleColor.Red);
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+
+    Console.WriteLine("Write the second number:");
+    string? secondNumber = Console.ReadLine();
+    float number2;
+    bool isNumber2 = float.TryParse(secondNumber, out number2);
+
+    if (!isNumber2)
+    {
+        WriteInColor($"{secondNumber} n'est pas un nombre", ConsoleColor.Red);
+        Console.ReadLine();
+        Console.Clear();
+        continue;
+    }
+
+    if (isNumber1 && isNumber2) WriteInColor($"{number1} + {number2} = {number1 + number2}", ConsoleColor.Green);
+    break;
+}
+
+void WriteInColor(string input, ConsoleColor color, bool jumpLine = true)
+{
+    Console.ForegroundColor = color;
+    if (jumpLine) Console.WriteLine(input);
+    else Console.Write(input);
+    Console.ResetColor();
+}
+
+#endregion
---
+++ Exercice2/Program.cs
@@ -0,0 +1,88 @@
+﻿#region Conditions
+
+bool isGameDone = false;
+
+while(!isGameDone)
+{
+    int upperLimit;
+    Console.WriteLine("Veuillez choisir un niveau de difficulté entre 1 (facile), 2 (moyen) ou 3 (difficile):");
+
+    if (!int.TryParse(Console.ReadLine(), out int level))
+    {
+        WriteInColor("veuillez entrer un nombre", ConsoleColor.Red);
+    }
+    else 
+    {
+        switch (level)
+        {
+            case 1:
+                upperLimit = 50;
+                WriteInColor("Bienvenue dans le mode facile", ConsoleColor.Green);
+                break;
+            case 2:
+                upperLimit = 100;
+                WriteInColor("Bienvenue dans le mode normal", ConsoleColor.Yellow);
+                break;
+            case 3:
+                upperLimit = 200;
+                WriteInColor("Bienvenue dans le mode difficile", ConsoleColor.Red);
+                break;
+            default:
+                WriteInColor("le nombre entré n'est aucun des bons nombres", ConsoleColor.Red);
+                continue;
+        }
+
+        StartGame(upperLimit);
+        isGameDone = true;
+    }
+}
+
+void StartGame(int upperLimit)
+{
+    Random random = new Random();
+    int numberToFind = random.Next(0, upperLimit + 1);
+    
+    bool isGameWon = false;
+
+    while (!isGameWon)
+    {
+        Console.WriteLine($"Devine le nombre entre 0 et {upperLimit}");
+        int guessedNumber;
+        bool isANumber = int.TryParse(Console.ReadLine(), out guessedNumber);
+
+        if (!isANumber) continue;
+        
+        if (guessedNumber != numberToFind)
+        {
+            WriteInColor("C'est pas le bon :(", ConsoleColor.Red);
+            GuidePlayer(guessedNumber, numberToFind);
+        }
+        else
+        {
+            WriteInColor("Woohoo!!! Tu as gagné Rien!", ConsoleColor.Green);
+            isGameWon = true;
+        }
+    }
+}
+
+void GuidePlayer(int guessedNumber, int numberToFind)
+{
+    if (guessedNumber < numberToFind)
+    {
+        WriteInColor("Plus grand!", ConsoleColor.Red);
+    }
+    else if (guessedNumber > numberToFind)
+    {
+        WriteInColor("Plus petit!", ConsoleColor.Red);
+    }
+}
+
+
+void WriteInColor(string input, ConsoleColor color, bool jumpLine = true)
+{
+    Console.ForegroundColor = color;
+    if (jumpLine) Console.WriteLine(input);
+    else Console.Write(input);
+    Console.ResetColor();
+}
+#endregion
